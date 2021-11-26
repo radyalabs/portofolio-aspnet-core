4ecf662f4cb0a76d66a9e079a6f48c1655c6ed64
Yanuar Wanda <yanuar.wanda2@gmail.com>
2021-11-26 14:30:05 +0700

    project init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace portofolio_aspnet_core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
